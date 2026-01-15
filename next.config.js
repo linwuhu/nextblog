209391b405762e71a07262431c84ab2b9359b887
linwuhu <925735487@qq.com>
2026-01-15 12:32:08 +0800

    refactor: 统一使用 NEXT_PUBLIC_BASE_PATH 作为基础路径配置

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	basePath: process.env.BASE_PATH,
+	basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 	output: "export",
 	images: {
 		unoptimized: true,
